b809bfc6db04e7de396f96a91d62efa210df5965
elietshibangu <elietshibangu347@gmail.com>
2020-06-15 19:22:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>LearnPro Dev</title>
+  </head>
+  <body>
+    <p>Bienvenue dans notre site!</p>
+  </body>
+</html>
